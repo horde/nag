horde/mnemo

Export owner and read-only information via WebDAV.

lib/Api.php
@@ -57,7 +57,7 @@ class Mnemo_Api extends Horde_Registry_Api
      */
     public function browse($path = '', $properties = array())
     {
-        global $registry;
+        global $injector, $mnemo_shares, $registry;
 
         // Default properties.
         if (!$properties) {
@@ -69,6 +69,7 @@ class Mnemo_Api extends Horde_Registry_Api
         }
         $path = trim($path, '/');
         $parts = explode('/', $path);
+        $currentUser = $registry->getAuth();
 
         if (empty($path)) {
             // This request is for a list of all users who have notepads
@@ -90,6 +91,8 @@ class Mnemo_Api extends Horde_Registry_Api
                 if (in_array('browseable', $properties)) {
                     $results['mnemo/' . $owner]['browseable'] = true;
                 }
+                if (in_array('read-only', $properties)) {
+                    $results['mnemo/' . $owner]['read-only'] = true;
                 }
             }
             return $results;
@@ -97,10 +100,11 @@ class Mnemo_Api extends Horde_Registry_Api
         } elseif (count($parts) == 1) {
             // This request is for all notepads owned by the requested user
             $owner = $parts[0] == '-system-' ? '' : $parts[0];
-            $notepads = $GLOBALS['mnemo_shares']->listShares(
-                $GLOBALS['registry']->getAuth(),
+            $notepads = $mnemo_shares->listShares(
+                $currentUser,
                 array('perm' => Horde_Perms::SHOW,
-                      'attributes' => $owner));
+                      'attributes' => $owner)
+            );
 
             $results = array();
             foreach ($notepads as $notepadId => $notepad) {
@@ -111,12 +115,17 @@ class Mnemo_Api extends Horde_Registry_Api
                 if (in_array('name', $properties)) {
                     $results[$retpath]['name'] = sprintf(_("Notes from %s"), Mnemo::getLabel($notepad));
                 }
+                if (in_array('owner', $properties)) {
+                    $results[$retpath]['owner'] = $notepad->get('owner') ?: '-system-';
+                }
                 if (in_array('icon', $properties)) {
                     $results[$retpath]['icon'] = Horde_Themes::img('mnemo.png');
                 }
                 if (in_array('browseable', $properties)) {
-                    $results[$retpath]['browseable'] = $notepad->hasPermission($GLOBALS['registry']->getAuth(), Horde_Perms::READ);
+                    $results[$retpath]['browseable'] = $notepad->hasPermission($currentUser, Horde_Perms::READ);
                 }
+                if (in_array('read-only', $properties)) {
+                    $results[$retpath]['read-only'] = !$notepad->hasPermission($currentUser, Horde_Perms::EDIT);
                 }
             }
             return $results;
@@ -126,10 +135,17 @@ class Mnemo_Api extends Horde_Registry_Api
             // This request is browsing into a specific notepad.  Generate the
             // list of items and represent them as files within the directory.
             //
-            if (!Mnemo::hasPermission($parts[1], Horde_Perms::READ)) {
+            try {
+                $notepad = $mnemo_shares->getShare($parts[1]);
+            } catch (Horde_Exception_NotFound $e) {
+                throw new Mnemo_Exception(_("Invalid notepad requested."), 404);
+            } catch (Horde_Share_Exception $e) {
+                throw new Mnemo_Exception($e->getMessage, 500);
+            }
+            if (!$notepad->hasPermission($currentUser, Horde_Perms::READ)) {
                 throw new Mnemo_Exception(_("Invalid notepad requested."), 404);
             }
-            $storage = $GLOBALS['injector']->getInstance('Mnemo_Factory_Driver')->create($parts[1]);
+            $storage = $injector->getInstance('Mnemo_Factory_Driver')->create($parts[1]);
             try {
                 $storage->retrieve();
             } catch (Mnemo_Exception $e) {
@@ -145,11 +161,17 @@ class Mnemo_Api extends Horde_Registry_Api
                 if (in_array('name', $properties)) {
                     $results[$key]['name'] = $memo['desc'];
                 }
+                if (in_array('owner', $properties)) {
+                    $results[$key]['owner'] = $notepad->get('owner') ?: '-system-';
+                }
                 if (in_array('icon', $properties)) {
                     $results[$key]['icon'] = $icon;
                 }
                 if (in_array('browseable', $properties)) {
                     $results[$key]['browseable'] = false;
+                }
+                if (in_array('read-only', $properties)) {
+                    $results[$key]['read-only'] = !$notepad->hasPermission($currentUser, Horde_Perms::EDIT);
                 }
                 if (in_array('contenttype', $properties)) {
                     $results[$key]['contenttype'] = 'text/plain';
@@ -178,7 +200,8 @@ class Mnemo_Api extends Horde_Registry_Api
                 // This request is for a specific item within a given notepad.
                 //
                 /* Create a Mnemo storage instance. */
-                $storage = $GLOBALS['injector']->getInstance('Mnemo_Factory_Driver')->create($parts[1]);
+                $storage = $injector->getInstance('Mnemo_Factory_Driver')
+                    ->create($parts[1]);
                 $storage->retrieve();
                 try {
                     $memo = $storage->get($parts[2]);

horde/nag

Conflicts:
	mnemo/lib/Api.php
	turba/lib/Api.php

lib/Api.php
@@ -244,7 +244,7 @@ class Nag_Api extends Horde_Registry_Api
      */
     public function browse($path = '', $properties = array())
     {
-        global $registry;
+        global $injector, $nag_shares, $registry;
 
         // Default properties.
         if (!$properties) {
@@ -256,6 +256,7 @@ class Nag_Api extends Horde_Registry_Api
         }
         $path = trim($path, '/');
         $parts = explode('/', $path);
+        $currentUser = $registry->getAuth();
 
         if (empty($path)) {
             // This request is for a list of all users who have tasklists
@@ -277,14 +278,17 @@ class Nag_Api extends Horde_Registry_Api
                 if (in_array('browseable', $properties)) {
                     $results['nag/' . $owner]['browseable'] = true;
                 }
+                if (in_array('read-only', $properties)) {
+                    $results['nag/' . $owner]['read-only'] = true;
+                }
             }
             return $results;
 
         } elseif (count($parts) == 1) {
             // This request is for all tasklists owned by the requested user
             $owner = $parts[0] == '-system-' ? '' : $parts[0];
-            $tasklists = $GLOBALS['nag_shares']->listShares(
-                $GLOBALS['registry']->getAuth(),
+            $tasklists = $nag_shares->listShares(
+                $currentUser,
                 array('perm' => Horde_Perms::SHOW,
                       'attributes' => $owner));
 
@@ -298,13 +302,20 @@ class Nag_Api extends Horde_Registry_Api
                     $results[$retpath]['name'] = sprintf(_("Tasks from %s"), Nag::getLabel($tasklist));
                     $results[$retpath . '.ics']['name'] = Nag::getLabel($tasklist);
                 }
+                if (in_array('owner', $properties)) {
+                    $results[$retpath]['owner'] = $tasklist->get('owner') ?: '-system-';
+                    $results[$retpath . '.ics']['owner'] = $tasklist->get('owner') ?: '-system-';
+                }
                 if (in_array('icon', $properties)) {
                     $results[$retpath]['icon'] = Horde_Themes::img('nag.png');
                     $results[$retpath . '.ics']['icon'] = Horde_Themes::img('mime/icalendar.png');
                 }
                 if (in_array('browseable', $properties)) {
-                    $results[$retpath]['browseable'] = $tasklist->hasPermission($GLOBALS['registry']->getAuth(), Horde_Perms::READ);
+                    $results[$retpath]['browseable'] = $tasklist->hasPermission($currentUser, Horde_Perms::READ);
                     $results[$retpath . '.ics']['browseable'] = false;
+                }
+                if (in_array('read-only', $properties)) {
+                    $results[$retpath]['read-only'] = $results[$retpath . '.ics']['read-only'] = !$tasklist->hasPermission($currentUser, Horde_Perms::EDIT);
                 }
                 if (in_array('contenttype', $properties)) {
                     $results[$retpath . '.ics']['contenttype'] = 'text/calendar';
@@ -330,13 +341,20 @@ class Nag_Api extends Horde_Registry_Api
 
         } elseif (count($parts) == 2) {
             //
-            // This request is browsing into a specific tasklist.  Generate the list
-            // of items and represent them as files within the directory.
+            // This request is browsing into a specific tasklist.  Generate the
+            // list of items and represent them as files within the directory.
             //
-            if (!Nag::hasPermission($parts[1], Horde_Perms::READ)) {
+            try {
+                $tasklist = $nag_shares->getShare($parts[1]);
+            } catch (Horde_Exception_NotFound $e) {
+                throw new Nag_Exception(_("Invalid task list requested."), 404);
+            } catch (Horde_Share_Exception $e) {
+                throw new Nag_Exception($e->getMessage, 500);
+            }
+            if (!$tasklist->hasPermission($currentUser, Horde_Perms::READ)) {
                 throw new Nag_Exception(_("Invalid task list requested."), 404);
             }
-            $storage = $GLOBALS['injector']->getInstance('Nag_Factory_Driver')->create($parts[1]);
+            $storage = $injector->getInstance('Nag_Factory_Driver')->create($parts[1]);
             try {
                 $storage->retrieve();
             } catch (Nag_Exception $e) {
@@ -350,11 +368,17 @@ class Nag_Api extends Horde_Registry_Api
                 if (in_array('name', $properties)) {
                     $results[$key]['name'] = $task->name;
                 }
+                if (in_array('owner', $properties)) {
+                    $results[$key]['owner'] = $tasklist->get('owner') ?: '-system-';
+                }
                 if (in_array('icon', $properties)) {
                     $results[$key]['icon'] = $icon;
                 }
                 if (in_array('browseable', $properties)) {
                     $results[$key]['browseable'] = false;
+                }
+                if (in_array('read-only', $properties)) {
+                    $results[$key]['read-only'] = !$tasklist->hasPermission($currentUser, Horde_Perms::EDIT);
                 }
                 if (in_array('contenttype', $properties)) {
                     $results[$key]['contenttype'] = 'text/calendar';
@@ -377,7 +401,8 @@ class Nag_Api extends Horde_Registry_Api
                 // This request is for a specific item within a given task list.
                 //
                 /* Create a Nag storage instance. */
-                $storage = $GLOBALS['injector']->getInstance('Nag_Factory_Driver')->create($parts[1]);
+                $storage = $injector->getInstance('Nag_Factory_Driver')
+                    ->create($parts[1]);
                 $storage->retrieve();
                 try {
                     $task = $storage->get($parts[2]);