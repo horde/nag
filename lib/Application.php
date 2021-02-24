450301faed10d76deb53127ce4d7590ed7179ee1
Ralf Lang <lang@b1-systems.de>
2021-02-24 15:45:39 +0000

    Released nag-5.0.0alpha1

--- lib/Application.php
+++ lib/Application.php
@@ -44,7 +44,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0alpha1';
+    public $version = 'H5 (5.0.0alpha1)';
 
     /**
      * Global variables defined:
