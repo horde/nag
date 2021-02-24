d69ceb19c1c7da546fad0c44685c8b2423e393f1
Ralf Lang <lang@b1-systems.de>
2021-02-24 15:45:39 +0000

    Development mode for nag-5.0.0alpha3

--- lib/Application.php
+++ lib/Application.php
@@ -44,7 +44,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (5.0.0alpha1)';
+    public $version = 'H5 (5.0.0alpha2)';
 
     /**
      * Global variables defined:
