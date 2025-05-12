61f948fe07ac888f299a5b3c83956aefb3d5a97b
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-12 22:06:59 +0200

    Release 5.0.0-alpha7  (API Version: 5.0.0-alpha1)
    ci: Add modern pipeline though it still fails
    style: Apply php-cs-fixer

--- lib/Application.php
+++ lib/Application.php
@@ -45,7 +45,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-alpha6';
+    public $version = '5.0.0-alpha7';
 
     /**
      * Global variables defined:
