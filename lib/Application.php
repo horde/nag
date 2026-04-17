3308a57fcd7fec278265c9627fe4fc4425d0180e
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-17 15:21:31 +0200

    chore(release): bump version to 5.0.0-beta4
    Release version 5.0.0-beta4
    
    fix: This is fatal in php 8.5
    docs: create/update conf.php.dist
    docs: Provide config example
    refactor: Remove most reliance on prototype.js
    fix: More defensive code when trying to drop tables
    fix: Accept both types of Horde Variable in turba
    feat(kolab)!: Remove Kolab backends
    feat(kolab)!: Remove Kolab options from config xml
    chore: It was good while it lasted

--- lib/Application.php
+++ lib/Application.php
@@ -46,7 +46,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta3';
+    public $version = '5.0.0-beta4';
 
     /**
      * Global variables defined:
